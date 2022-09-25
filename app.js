e4c77d790cd96f292018c061ae5433fa3e99d6cd
root <root@Phoebe.localdomain>
2022-09-24 21:54:23 -0400

    second

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+// ES5 Global Constants
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+// ES2015 Global Constants
+const PI = 3.14; 
+PI=42 // error
+
+// Quiz
+// What is the difference between var and let?
+
+// var can be reassigned and redeclared, and is function scoped. Let cannot be redeclared and makes a block scope. 
+
+// What is the difference between var and const?
+
+// var can be reassigned and redeclared, and is function scoped. const cannot be redeclared or reassigned and it's block scoped. 
+// What is the difference between let and const?
+// let can be reassgined, const can't. 
+
+// What is hoisting?
+// when js prioritizes a declaration over intialization. 
